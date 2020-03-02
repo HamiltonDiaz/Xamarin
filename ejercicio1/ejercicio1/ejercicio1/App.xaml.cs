91a06e95f8f222505a1a3ca94156e36bc456cb36
HamiltonDiaz <54079326+HamiltonDiaz@users.noreply.github.com>
2020-03-01 22:20:18 -0500

    ok
    ok

---
+++ ejercicio1/ejercicio1/ejercicio1/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ejercicio1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            //cuando la applicación inicia
+        }
+
+        protected override void OnSleep()
+        {
+            //cuando la applicación está en segundo plano
+        }
+
+        protected override void OnResume()
+        {
+            //cuando está en segundo plano y se vuelve activar
+        }
+    }
+}
